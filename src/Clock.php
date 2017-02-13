cedd58cacdd059a9e157af430fd8a581bfb927aa
Andrew Devlin and Charles Peden <charles@>
2017-02-13 15:20:29 -0800

    add spec to pass 3rd test

--- src/Clock.php
+++ src/Clock.php
@@ -1,12 +1,34 @@
 <?php
     class Clock {
-        function findAngle($input)
+        function findHourAngle($input)
         {
             $hour = $input[0];
 
-            $output = $hour * 30;
+            $hourOutput = $hour * 30;
 
-            return $output;
+            return $hourOutput;
+        }
+
+        function findMinuteAngle($input)
+        {
+            $minute = $input[1];
+            $minuteOutput = $minute * 6;
+
+            return $minuteOutput;
+        }
+
+        function compareAngle($input)
+        {
+            $hourAngle = Clock::findHourAngle($input);
+            $minuteAngle = Clock::findMinuteAngle($input);
+
+            if($hourAngle > $minuteAngle)
+            {
+                $resultAngle = $hourAngle - $minuteAngle;
+            } else {
+                $resultAngle = $minuteAngle - $hourAngle;
+            }
+            return $resultAngle;
         }
     }
 ?>
--- tests/ClockTest.php
+++ tests/ClockTest.php
@@ -8,8 +8,27 @@
             $test_Clock = new Clock;
             $input = [1,00];
 
-            $result = $test_Clock->findAngle($input);
+            $result = $test_Clock->findHourAngle($input);
             $this->assertEquals(30, $result);
         }
+
+        function test_convertMinutes()
+        {
+            $test_Clock = new Clock;
+            $input = [1,25];
+
+            $result = $test_Clock->findMinuteAngle($input);
+            $this->assertEquals(150, $result);
+        }
+
+        function test_compareAngle()
+        {
+            $test_Clock = new Clock;
+            $input = [1,25];
+
+            $result = $test_Clock->compareAngle($input);
+            $this->assertEquals(120, $result);
+        }
+
     }
 ?>
